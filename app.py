c4c46de8e4a8dcb5f0943efc72e0c5a5edf2fb3f
Nick Bennett <nick@goggl.es>
2014-04-12 00:18:25 -0500

    Make new /urls page with Twitter Bootstrap.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Standard Library
+import codecs
 from datetime import datetime
 import os
 import re
@@ -8,8 +9,7 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import requests
-# Local Imports
-from config import SECRET_KEY
+
 
 # Regex from http://stackoverflow.com/a/169631
 image_url_pattern = (r'https?://(?:[a-z\-]+\.)+[a-z]{2,6}'
@@ -43,29 +43,57 @@ def get_image_urls_from_html(html, base_url=None):
             [img.attrs.get('data-rollover') for img in imgs])))
     if base_url is not None:
         image_urls = [urljoin(base_url, url) for url in image_urls]
-    return sorted(image_urls)
+    return sorted(list(set(image_urls)))
+
+
+def get_url(url):
+    cache = db.session.query(PageCache).filter_by(url=url).first()
+    if cache:
+        html = cache.html
+    else:
+        response = requests.get(url)
+        html = response.text
+        cache = PageCache(url, html)
+        db.session.add(cache)
+        db.session.commit()
+    return html    
 
 
 @app.route('/', methods=['GET'])
 def index():
-    key = request.args.get('key')
     url = request.args.get('url')
-    if key and url and key == SECRET_KEY:
-        cache = db.session.query(PageCache).filter_by(url=url).first()
-        if cache:
-            html = cache.html
-        else:
-            response = requests.get(url)
-            html = response.content
-            cache = PageCache(url, html)
-            db.session.add(cache)
-            db.session.commit()
-        retrieval_date = datetime.isoformat(cache.created)
+    if url:
+        html = get_url(url)
         image_urls = get_image_urls_from_html(html, base_url=url)
-        return render_template('index.html', url=url, 
-            image_urls=image_urls, retrieval_date=retrieval_date)
+        return render_template('index.html', url=url, image_urls=image_urls)
     else:
         return 'o_O'
+
+
+@app.route('/urls', methods=['GET'])
+def urls():
+    url = request.args.get('url')
+    show_links = (request.args.get('show_links') == 'on')
+    show_images = (request.args.get('show_images') == 'on')
+    if url:
+        html = get_url(url)
+        image_urls = get_image_urls_from_html(html, base_url=url)
+        return render_template('urls.html', urls=image_urls,
+            show_links=show_links, show_images=show_images)
+    else:
+        return render_template('get_urls.html')
+
+
+@app.route('/paste', methods=['GET', 'POST'])
+def paste():
+    if request.method=='GET':
+        return render_template('paste.html')
+    else:
+        url = request.form['url']
+        html = request.form['html']
+        image_urls = get_image_urls_from_html(html, base_url=url)
+        return render_template('index.html', url=url, image_urls=image_urls)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
