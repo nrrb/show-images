0bd17d90b4997bf64597e2a796d77b99d0417608
Nick Bennett <nick@goggl.es>
2014-04-16 22:39:38 -0500

    Add image URL cache, history page, some other junk.

--- app.py
+++ app.py
@@ -30,20 +30,30 @@ class PageCache(db.Model):
         self.created = datetime.now()
 
 
+class image_urls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    page_url = db.Column(db.String(500))
+    image_url = db.Column(db.String(500))
+
+    def __init__(self, page_url, image_url):
+        self.page_url = page_url
+        self.image_url = image_url
+
+
 def get_image_urls_from_html(html, base_url=None):
     # Search by regular expression first
-    image_urls = re.findall(image_url_pattern, html)
+    urls = re.findall(image_url_pattern, html)
     # Then use BeautifulSoup to extract those pesky image tags
     soup = BeautifulSoup(html)
     imgs = soup.findAll('img')
-    image_urls += list(set(filter(lambda a: a,
+    urls += list(set(filter(lambda a: a,
             [img.attrs.get('src') for img in imgs])))
     # Sometimes there's an interesting data-rollover on images
-    image_urls += list(set(filter(lambda a: a, 
+    urls += list(set(filter(lambda a: a,
             [img.attrs.get('data-rollover') for img in imgs])))
     if base_url is not None:
-        image_urls = [urljoin(base_url, url) for url in image_urls]
-    return sorted(list(set(image_urls)))
+        urls = [urljoin(base_url, url) for url in urls]
+    return sorted(list(set(urls)))
 
 
 def get_url(url):
@@ -56,7 +66,7 @@ def get_url(url):
         cache = PageCache(url, html)
         db.session.add(cache)
         db.session.commit()
-    return html    
+    return html
 
 
 @app.route('/', methods=['GET'])
@@ -64,8 +74,8 @@ def index():
     url = request.args.get('url')
     if url:
         html = get_url(url)
-        image_urls = get_image_urls_from_html(html, base_url=url)
-        return render_template('index.html', url=url, image_urls=image_urls)
+        urls = get_image_urls_from_html(html, base_url=url)
+        return render_template('index.html', url=url, image_urls=urls)
     else:
         return 'o_O'
 
@@ -77,11 +87,21 @@ def urls():
     show_images = (request.args.get('show_images') == 'on')
     if url:
         html = get_url(url)
-        image_urls = get_image_urls_from_html(html, base_url=url)
-        return render_template('urls.html', urls=image_urls,
+        urls = get_image_urls_from_html(html, base_url=url)
+        for image_url in urls:
+            url_cache = image_urls(page_url=url, image_url=image_url)
+            db.session.add(url_cache)
+            db.session.commit()
+        return render_template('urls.html', urls=urls,
             show_links=show_links, show_images=show_images)
     else:
         return render_template('get_urls.html')
+
+
+@app.route('/history', methods=['GET'])
+def history():
+    urls = [item.url for item in db.session.query(PageCache).all()]
+    return render_template('history.html', urls=urls)
 
 
 @app.route('/paste', methods=['GET', 'POST'])
@@ -91,8 +111,8 @@ def paste():
     else:
         url = request.form['url']
         html = request.form['html']
-        image_urls = get_image_urls_from_html(html, base_url=url)
-        return render_template('index.html', url=url, image_urls=image_urls)
+        urls = get_image_urls_from_html(html, base_url=url)
+        return render_template('index.html', url=url, image_urls=urls)
 
 
 if __name__ == '__main__':
